4085c3eabe387f694c5015cc1ed5c253ba03798d
haeun82 <cjwd829@gmail.com>
2024-03-18 21:48:58 +0900

    라이브러리 세팅

---
+++ src/components/main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+  const TopWrapper = styled.div`
+    max-width: 100%;
+    max-height: 700px;
+  `;
+function main(props) {
+  return (
+    <TopWrapper>
+      <h1>HAEUN PORTFOLIO</h1>
+    </TopWrapper>
+  );
+}
+
+export default main;

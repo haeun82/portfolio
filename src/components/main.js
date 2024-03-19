f2d784846df1526a83d5d67fe7242eaa6757646e
haeun82 <cjwd829@gmail.com>
2024-03-19 20:21:26 +0900

    텍스트 위치

--- src/components/main.js
+++ src/components/main.js
@@ -2,12 +2,53 @@ import React from 'react';
 import styled from 'styled-components';
   const TopWrapper = styled.div`
     max-width: 100%;
-    max-height: 700px;
+    height: 800px;
+    background-color: gray;
   `;
+
+  const TopText = styled.div`
+    max-width: 100%;
+    height: 90%;
+    display: flex;
+    justify-content: space-between;
+    padding-top: 50px;
+    h1 {
+      font-size: 50px;
+    }
+
+    h5 {
+      font-size: 25px;
+      text-align: right;
+      align-self: flex-end;
+    }
+
+    .border {
+      width: 100px;
+      height: 35px;
+      /* padding: 5px; */
+      border: 1px solid black;
+      border-radius: 20px;
+
+      p {
+        font-size: 20px;
+        font-weight: bold;
+        color: #fff;
+        text-align: center;
+        line-height: 1.6;
+      }
+    }
+  `;
+
 function Main(props) {
   return (
     <TopWrapper>
-      <h1>HAEUN PORTFOLIO</h1>
+      <TopText>
+        <h1>FRONTEND <br />PORTFOLIO</h1>
+        <h5>HAN HA EUN</h5>
+        <div className='border'>
+          <p>2024</p>
+        </div>
+      </TopText>
     </TopWrapper>
   );
 }
